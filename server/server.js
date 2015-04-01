959a0e19ce06e85d007794bd613d172f3e5aab98
Joey Carson <deathdancefloor@gmail.com>
2015-04-01 19:57:15 -0400

    add special endpoint

--- server/server.js
+++ server/server.js
@@ -5,8 +5,17 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+// A route is a combination of a URI, a HTTP request method (GET, POST, and so on), and one or more 
+// handlers for the endpoint. It takes the following structure app.METHOD(path, [callback...], callback),
+// where app is an instance of express, METHOD is an HTTP request method, path is a path on the server, 
+// and callback is the function executed when the route is matched.
 app.get('/', function(request, response) {
-  //response.send('Hello World!');
+  console.log("hit root");
+  response.send(cool());
+});
+
+app.get('/special', function(request, response) {
+  console.log("hit special");
   response.send(cool());
 });
 
