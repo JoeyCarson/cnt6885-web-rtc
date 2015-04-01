ea567b73ae8a8432ceb67eedd4846efde882c363
Joey Carson <deathdancefloor@gmail.com>
2015-04-01 19:21:45 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-var http = require("http");
-
-
-console.log("starting server");
-
-http.createServer(function(request, response) {
+function onRequest(request, response) {
 
 	console.log("request received");  
 
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
-}).listen(8888);
+}
+
+
+var http = require("http");
+console.log("starting server");
+http.createServer(onRequest).listen(8888);
