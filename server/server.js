59579d37f22775e68d0f39d3f701ac55c843bffe
Joey Carson <deathdancefloor@gmail.com>
2015-04-01 20:14:40 -0400

    server server server

--- server/server.js
+++ server/server.js
@@ -2,23 +2,22 @@ var express = require('express');
 var cool =  require("cool-ascii-faces");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 
 // A route is a combination of a URI, a HTTP request method (GET, POST, and so on), and one or more 
 // handlers for the endpoint. It takes the following structure app.METHOD(path, [callback...], callback),
 // where app is an instance of express, METHOD is an HTTP request method, path is a path on the server, 
 // and callback is the function executed when the route is matched.
-app.get('/', function(request, response) {
-  console.log("hit root");
-  response.send(cool());
-});
+function registerPeer(request, response) {
+  console.log("registerPeer %o", request);
+  response.send("hello");
+}
 
-app.get('/special', function(request, response) {
-  console.log("hit special");
-  response.send(cool());
-});
 
+app.use(express.static(__dirname + '/public'));
+app.get('/register', registerPeer);
+
+// Listen locally.
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
